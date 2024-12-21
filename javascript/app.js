3716ee070ec391e74551ae3748d1fe732e91ec69
Lucas Pinheiro Machado <lucaslpma10@gmail.com>
2024-12-20 23:06:17 -0300

    correção ortografia

--- javascript/app.js
+++ javascript/app.js
@@ -1,15 +1,12 @@
 document.querySelectorAll('.read-more').forEach(button => {
   button.addEventListener('click', () => {
     const project = button.closest('.project');
-    const text = project.querySelector('p'); // Seleciona o parágrafo dentro do projeto
+    const text = project.querySelector('p');
     
-    // Alterna entre expandir e colapsar o texto
     if (text.classList.contains('expanded')) {
-      // Se o texto estiver expandido, recolhe
       text.classList.remove('expanded');
       button.textContent = '...Ler mais';
     } else {
-      // Se o texto estiver colapsado, expande
       text.classList.add('expanded');
       button.textContent = 'Ler menos';
     }
